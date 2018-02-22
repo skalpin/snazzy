333684469047981834efd5d21ec5a22dd83320c4
Stephen Kalpin <skalpin@gmail.com>
2018-02-21 20:07:56 -0600

    Updating temp_watch

--- temp_watch.py
+++ temp_watch.py
@@ -3,8 +3,8 @@ import time
 
 def measure_temp():
 	temp = os.popen("vcgencmd measure_temp").readline()
-	return (temp.replace("temp=",""))
+	return temp.replace('\r', "").replace('\n', "").replace("temp=","")
 
 while True:
-	print(measure_temp())
-	time.sleep(1)
+	print('\r' + measure_temp(), end="\r", flush=True)
+	time.sleep(.1)
